456169a31246255dcc72fd90a4133e5f0fb80158
daniel1006 <71275163+daniel1006@users.noreply.github.com>
2020-11-19 15:53:53 +0100

    Update MessageBox.js

--- MessageBox.js
+++ MessageBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './MessageBox.css';
 
 function MessageBox(props) {
-    const classes = `Message${props.error ? ' Error' : ''}`; // I think the return function displays the actual message ?
+    const classes = `Message${props.error ? ' Error' : ''}`; // I think the return function displays the actual message saying whether or not there is an error?
     return <div className={classes}>{props.message}</div>;
 }
 
